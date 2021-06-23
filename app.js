f58ccb5fa8a536f7cabaf2a8767e0faf8c83c448
Caroline Ong <carolineong@Carolines-MacBook-Pro.local>
2021-06-23 10:41:23 -0400

    added JS functions

--- app.js
+++ app.js
@@ -3,3 +3,23 @@
 // When a character name is searched, quote pops up
 // Make Sure Previous Quotes are removed
 // 
+
+const URL = 'https://animechan.vercel.app/api/random'
+const searchURL = `https://animechan.vercel.app/api/quotes/character?name=`
+
+let characterSearch = document.querySelector('#search-name')
+let randomSearch = document.querySelector('#random')
+let searchButton = document.querySelector('input')
+let quoteData = document.querySelector('.quotes')
+
+function quotes() {
+  console.log(searchButton.value)
+  characterQuote(searchButton)
+}
+let characterQuote = async (searchButton) => {
+  try {
+    characterQuote = await axios.get(`${searchURL}$s=${searchButton.value}`)
+  } catch (error) {
+    console.log(error)
+  }
+}
